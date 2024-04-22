76cde3549267ed635f91b52a9d0915b5c442ad04
Douwe Somers <douwe.somers@student.kuleuven.be>
2024-04-22 19:39:15 +0200

    Adder wrapper for timing

--- cdd_n_bit_adder.runs/synth_1/wrap_adder.tcl
+++ cdd_n_bit_adder.runs/synth_1/wrap_adder.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.runs/synth_1/design_1_wrapper.tcl"
+  variable script "C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.runs/synth_1/wrap_adder.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,29 +70,29 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 4
-set_param xicom.use_bs_reader 1
-set_msg_config -id {HDL-1065} -limit 10000
+set_param synth.incrementalSynthesisCache C:/Users/douwe/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-35568-Omen-17/incrSyn
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7z020clg400-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir {C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.cache/wt} [current_project]
-set_property parent.project_path {C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.xpr} [current_project]
+set_property webtalk.parent_dir {C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.cache/wt} [current_project]
+set_property parent.project_path {C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.xpr} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property board_part tul.com.tw:pynq-z2:part0:1.0 [current_project]
-set_property ip_output_repo {c:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.cache/ip} [current_project]
+set_property ip_output_repo {c:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_verilog -library xil_defaultlib {{C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v}}
-add_files {{C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.srcs/sources_1/bd/design_1/design_1.bd}}
-set_property used_in_implementation false [get_files -all {{C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.srcs/sources_1/bd/design_1/design_1_ooc.xdc}}]
-
+read_verilog -library xil_defaultlib {
+  {C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.srcs/sources_1/new/full_adder.v}
+  {C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.srcs/sources_1/new/ripple_carry_adder_Nb.v}
+  {C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.srcs/sources_1/new/wrap_adder.v}
+}
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -102,26 +102,24 @@ OPTRACE "Adding files" END { }
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc {{C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.srcs/constrs_1/imports/Tijdelijk/PYNQ-Z2v1.0.xdc}}
-set_property used_in_implementation false [get_files {{C:/Users/douwe/Documents/Vivado Projects/cdd_lab2/cdd_lab2.srcs/constrs_1/imports/Tijdelijk/PYNQ-Z2v1.0.xdc}}]
+read_xdc {{C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.srcs/constrs_1/imports/Tijdelijk/PYNQ-Z2v1.0.xdc}}
+set_property used_in_implementation false [get_files {{C:/Users/douwe/Documents/Vivado Projects/cdd_n_bit_adder/cdd_n_bit_adder.srcs/constrs_1/imports/Tijdelijk/PYNQ-Z2v1.0.xdc}}]
 
-read_xdc dont_touch.xdc
-set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
-synth_design -top design_1_wrapper -part xc7z020clg400-1
+synth_design -top wrap_adder -part xc7z020clg400-1
 OPTRACE "synth_design" END { }
 
 
 OPTRACE "write_checkpoint" START { CHECKPOINT }
 # disable binary constraint mode for synth run checkpoints
 set_param constraints.enableBinaryConstraints false
-write_checkpoint -force -noxdef design_1_wrapper.dcp
+write_checkpoint -force -noxdef wrap_adder.dcp
 OPTRACE "write_checkpoint" END { }
 OPTRACE "synth reports" START { REPORT }
-create_report "synth_1_synth_report_utilization_0" "report_utilization -file design_1_wrapper_utilization_synth.rpt -pb design_1_wrapper_utilization_synth.pb"
+create_report "synth_1_synth_report_utilization_0" "report_utilization -file wrap_adder_utilization_synth.rpt -pb wrap_adder_utilization_synth.pb"
 OPTRACE "synth reports" END { }
 file delete __synthesis_is_running__
 close [open __synthesis_is_complete__ w]
--- cdd_n_bit_adder.srcs/constrs_1/imports/Tijdelijk/PYNQ-Z2v1.0.xdc
+++ cdd_n_bit_adder.srcs/constrs_1/imports/Tijdelijk/PYNQ-Z2v1.0.xdc
@@ -6,7 +6,8 @@
 ## Clock signal 125 MHz
 
 set_property -dict { PACKAGE_PIN H16   IOSTANDARD LVCMOS33 } [get_ports iClk]; #IO_L13P_T2_MRCC_35 Sch=sysclk
-create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports iClk];
+create_clock -period 8.000 -name sys_clk_pin -waveform {0.000 4.000} -add [get_ports iClk]
+
 
 ##Switches
 
@@ -23,9 +24,8 @@ create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports iClk
 #set_property -dict { PACKAGE_PIN M15   IOSTANDARD LVCMOS33 } [get_ports { led5_r }]; #IO_L23N_T3_35 Sch=led5_r
 
 ##LEDs
-
-#set_property -dict { PACKAGE_PIN R14   IOSTANDARD LVCMOS33 } [get_ports { led[0] }]; #IO_L6N_T0_VREF_34 Sch=led[0]
-#set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33 } [get_ports { led[1] }]; #IO_L6P_T0_34 Sch=led[1]
+set_property -dict {PACKAGE_PIN R14 IOSTANDARD LVCMOS33} [get_ports oXORResult]; #IO_L6N_T0_VREF_34 Sch=led[0]
+set_property -dict {PACKAGE_PIN P14 IOSTANDARD LVCMOS33} [get_ports oCarryOut]; #IO_L6P_T0_34 Sch=led[1]
 #set_property -dict { PACKAGE_PIN N16   IOSTANDARD LVCMOS33 } [get_ports { led[2] }]; #IO_L21N_T3_DQS_AD14N_35 Sch=led[2]
 #set_property -dict { PACKAGE_PIN M14   IOSTANDARD LVCMOS33 } [get_ports { led[3] }]; #IO_L23P_T3_35 Sch=led[3]
 
